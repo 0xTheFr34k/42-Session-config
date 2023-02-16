fd178b1adf854940346ae2f112ca5c9c4e8c8d08
dev3beladed <dev3@beladed.com>
2023-02-16 01:07:57 +0100

    _code.py changed

--- _code.py
+++ _code.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 def code():
-    if os.system("code --version > /dev/null") != 0:
+    if os.system("code --version > /dev/null") == 1:
         code = "export PATH=\"$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin\""
         with open(os.path.expanduser("~/.zshrc"), "a") as f:
             f.write(code)
